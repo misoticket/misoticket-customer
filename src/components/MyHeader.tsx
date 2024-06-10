1377b7eb00358dc0c6a2b3913c3ba7a643bebab4
Jin <jwk70915@gmail.com>
2024-06-10 12:53:28 +0900

    fix: increase the 'market price' tab size

--- src/components/MyHeader.tsx
+++ src/components/MyHeader.tsx
@@ -56,18 +56,18 @@ export default function MyHeader() {
         <>
             {isMobile ? (
                 <div className="w-full fixed bg-white left-0 right-0 top-0 z-50">
-                    <div className="mx-4 py-3 flex justify-between items-center">
-                        <Image
-                            onClick={() => router.push("/")}
-                            src={logoImg}
-                            alt=""
-                            width={70}
-                            className="cursor-pointer"
-                        />
-                        <div className="flex items-center gap-4">
+                    <div className="mx-4 py-2 flex justify-between items-center">
+                        <div className="flex gap-4 items-center">
+                            <Image
+                                onClick={() => router.push("/")}
+                                src={logoImg}
+                                alt=""
+                                width={80}
+                                className="cursor-pointer"
+                            />
                             <div
                                 onClick={() => router.push("/market")}
-                                className="bg-gray-200 rounded-lg relative w-24 h-6 cursor-pointer hover:opacity-70"
+                                className="bg-gray-200 rounded-lg relative w-30 h-8 cursor-pointer hover:opacity-70"
                             >
                                 <Image
                                     className="w-full h-full rounded-lg"
@@ -75,11 +75,13 @@ export default function MyHeader() {
                                     alt=""
                                 />
                                 <div className="w-full h-full absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center">
-                                    <p className="font-semibold text-xs text-white">
+                                    <p className="font-semibold text-sm text-white">
                                         상품권 시세보기
                                     </p>
                                 </div>
                             </div>
+                        </div>
+                        <div className="flex items-center gap-4">
                             <Dropdown>
                                 <DropdownTrigger>
                                     <Image
@@ -134,14 +136,31 @@ export default function MyHeader() {
                 </div>
             ) : (
                 <div className="w-full fixed bg-white left-0 right-0 top-0 z-50">
-                    <div className="mx-10 py-3 flex justify-between items-center">
-                        <Image
-                            onClick={() => router.push("/")}
-                            src={logoImg}
-                            alt=""
-                            width={120}
-                            className="cursor-pointer"
-                        />
+                    <div className="mx-10 py-2 flex justify-between items-center">
+                        <div className="flex gap-8 items-center">
+                            <Image
+                                onClick={() => router.push("/")}
+                                src={logoImg}
+                                alt=""
+                                width={120}
+                                className="cursor-pointer"
+                            />
+                            <div
+                                onClick={() => router.push("/market")}
+                                className="bg-gray-200 rounded-lg relative w-32 h-10 cursor-pointer hover:opacity-70"
+                            >
+                                <Image
+                                    className="w-full h-full rounded-lg"
+                                    src={giftCardImg}
+                                    alt=""
+                                />
+                                <div className="w-full h-full absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center">
+                                    <p className="font-semibold text-base text-white">
+                                        상품권 시세보기
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
                         <div className="flex items-center gap-6">
                             <p
                                 className="font-medium text-sm cursor-pointer hover:opacity-50"
@@ -167,21 +186,6 @@ export default function MyHeader() {
                             >
                                 주문조회
                             </p>
-                            <div
-                                onClick={() => router.push("/market")}
-                                className="bg-gray-200 rounded-lg relative w-28 h-8 cursor-pointer hover:opacity-70"
-                            >
-                                <Image
-                                    className="w-full h-full rounded-lg"
-                                    src={giftCardImg}
-                                    alt=""
-                                />
-                                <div className="w-full h-full absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center">
-                                    <p className="font-semibold text-sm text-white">
-                                        상품권 시세보기
-                                    </p>
-                                </div>
-                            </div>
                         </div>
                     </div>
                     <div className="h-0.5 bg-gray-200"></div>
