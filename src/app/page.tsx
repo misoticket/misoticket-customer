691420498f04b1d5b65f89f8b820a2ac151a6409
Jin <jwk70915@gmail.com>
2024-07-05 21:02:49 +0900

    fix: remove mobile version temporarily

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import mainBanner from "@/../public/images/mainBanner.png";
-import mainBannerMobile from "@/../public/images/mainBannerMobile.png";
 import {
     fetchActiveBanner,
     fetchMainCategoryList,
@@ -48,37 +47,37 @@ export default function Home() {
             <main className="min-h-screen">
                 <MyHeader />
                 <CategoryTabBar selectedCategoryId={null} />
-                {isMobile ? (
+                <div>
                     <div>
-                        <div className="flex justify-center">
-                            {banner && (
-                                <div className="mt-28 bg-gray-100 w-full mx-4 px-8 py-4 rounded-lg">
-                                    <p className="font-medium text-base mb-4">
-                                        {banner.title}
-                                    </p>
-                                    <p className="font-medium text-xs whitespace-pre-line">
-                                        {banner.desc}
-                                    </p>
-                                </div>
-                            )}
+                        <div className="relative">
+                            <div className="flex justify-center">
+                                {banner && (
+                                    <div className="mt-40 bg-gray-100 w-1/2 px-8 py-4 rounded-lg border">
+                                        <p className="font-medium text-base mb-4">
+                                            {banner.title}
+                                        </p>
+                                        <p className="font-medium text-sm whitespace-pre-line">
+                                            {banner.desc}
+                                        </p>
+                                    </div>
+                                )}
+                            </div>
+                            <img
+                                className={`${
+                                    banner !== null ? "mt-4" : "mt-12"
+                                }`}
+                                src={mainBanner.src}
+                                alt=""
+                            />
                         </div>
-                        <img
-                            className={`${banner !== null ? "mt-4" : "mt-32"}`}
-                            src={mainBannerMobile.src}
-                            alt=""
-                        />
-                        <div
-                            className={`${
-                                banner ? "mt-12" : "mt-10"
-                            } flex justify-center`}
-                        >
-                            <div className="overflow-hidden">
+                        <div className="mt-16 flex justify-center">
+                            <div className="">
                                 {mainCategoryList.map((mc) => (
-                                    <div key={mc.id} className="mb-14">
-                                        <p className="font-semibold text-2xl mb-6 px-6">
+                                    <div key={mc.id} className="mb-20">
+                                        <p className="font-semibold text-2xl mb-6">
                                             {mc.name}
                                         </p>
-                                        <div className="flex gap-2 overflow-scroll px-6 scrollbar-hide">
+                                        <div className="grid grid-cols-4 gap-4">
                                             {mc.productIdList.map((prodId) => (
                                                 <ProductCell
                                                     key={prodId}
@@ -96,61 +95,7 @@ export default function Home() {
                             </div>
                         </div>
                     </div>
-                ) : (
-                    <div>
-                        <div>
-                            <div className="relative">
-                                <div className="flex justify-center">
-                                    {banner && (
-                                        <div className="mt-40 bg-gray-100 w-1/2 px-8 py-4 rounded-lg border">
-                                            <p className="font-medium text-base mb-4">
-                                                {banner.title}
-                                            </p>
-                                            <p className="font-medium text-sm whitespace-pre-line">
-                                                {banner.desc}
-                                            </p>
-                                        </div>
-                                    )}
-                                </div>
-                                <img
-                                    className={`${
-                                        banner !== null ? "mt-4" : "mt-12"
-                                    }`}
-                                    src={mainBanner.src}
-                                    alt=""
-                                />
-                            </div>
-                            <div className="mt-16 flex justify-center">
-                                <div className="">
-                                    {mainCategoryList.map((mc) => (
-                                        <div key={mc.id} className="mb-20">
-                                            <p className="font-semibold text-2xl mb-6">
-                                                {mc.name}
-                                            </p>
-                                            <div className="grid grid-cols-4 gap-4">
-                                                {mc.productIdList.map(
-                                                    (prodId) => (
-                                                        <ProductCell
-                                                            key={prodId}
-                                                            product={getProduct(
-                                                                prodId
-                                                            )}
-                                                            handleClick={() =>
-                                                                router.push(
-                                                                    `/product/${prodId}`
-                                                                )
-                                                            }
-                                                        />
-                                                    )
-                                                )}
-                                            </div>
-                                        </div>
-                                    ))}
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                )}
+                </div>
                 <MyFooter />
             </main>
         </>
