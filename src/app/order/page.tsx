6c3fbf9845638ef8c47dab73255f3733c750429f
Jin <jwk70915@gmail.com>
2024-07-22 14:01:21 +0900

    ⚡️ Add change address button to Order page

--- src/app/order/page.tsx
+++ src/app/order/page.tsx
@@ -784,14 +784,19 @@ export default function Page() {
                                                         </button>
                                                     ) : (
                                                         <div className="w-full">
-                                                            <p
-                                                                onClick={() =>
-                                                                    searchAddressModal.onOpen()
-                                                                }
-                                                                className="font-medium text-sm mb-2 cursor-pointer hover:opacity-60"
-                                                            >
-                                                                {address}
-                                                            </p>
+                                                            <div>
+                                                                <p className="font-medium text-sm mb-2">
+                                                                    {address}
+                                                                </p>
+                                                                <button
+                                                                    onClick={() =>
+                                                                        searchAddressModal.onOpen()
+                                                                    }
+                                                                    className="font-regular text-sm text-gray-500 bg-gray-200 rounded p-1 my-1"
+                                                                >
+                                                                    주소변경
+                                                                </button>
+                                                            </div>
                                                             <input
                                                                 value={
                                                                     addressDetail
@@ -1607,14 +1612,19 @@ export default function Page() {
                                                     </button>
                                                 ) : (
                                                     <div className="w-full">
-                                                        <p
-                                                            onClick={() =>
-                                                                searchAddressModal.onOpen()
-                                                            }
-                                                            className="font-medium text-sm mb-2 cursor-pointer hover:opacity-60"
-                                                        >
-                                                            {address}
-                                                        </p>
+                                                        <div className="flex items-center gap-4">
+                                                            <p className="font-medium text-sm mb-2">
+                                                                {address}
+                                                            </p>
+                                                            <button
+                                                                onClick={() =>
+                                                                    searchAddressModal.onOpen()
+                                                                }
+                                                                className="font-regular text-gray-500 bg-gray-200 rounded py-0.5 px-1 text-sm hover:opacity-60"
+                                                            >
+                                                                주소 변경
+                                                            </button>
+                                                        </div>
                                                         <input
                                                             value={
                                                                 addressDetail
